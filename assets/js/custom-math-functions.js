dd2dbc187788777031b0936fc55ad301abb8a975
lazyjobseeker <diadee1990@gmail.com>
2024-03-13 11:23:55 +0900

    Custom function updates

--- assets/js/custom-math-functions.js
+++ assets/js/custom-math-functions.js
@@ -7,3 +7,6 @@ function step(k, a, b, x) {
 function gibbs(k, x) {
   return 0.5*x*x*step(1000, 0, k, x) + Math.max(k*(x-k)+0.5*k*k, 0)*sigmoid(1000, (x-k));
 };
+function parabola(x) {
+  return x*x;
+};
