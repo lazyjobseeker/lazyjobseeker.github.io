0ea4c3e705cdaad2f2cb35143fd4f304ee8bc1fb
lazyjobseeker <diadee1990@gmail.com>
2024-09-16 02:38:42 +0900

    Visitor counter updated

---
+++ assets/js/visitorstat.js
@@ -0,0 +1,88 @@
+var dates = [];
+var counts = [];
+var step;
+var today = new Date();
+var size = 7;
+
+const ctx = document.getElementById('myChart');
+chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+    labels: dates,
+    datasets: [{
+        label: 'VISITORS',
+        data: counts,
+        borderWidth: 2,
+        pointBackgroundColor: '#42adff',
+        pointBorderColor: '#42adff',
+        borderColor: '#42adff',
+        pointRadius: 2.5,
+        cubicInterpolationMode: 'monotone',
+        tension: 0.4
+    }]
+    },
+    options: {
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+        scales: {
+            x: {
+                grid: {
+                    display: true,
+                }
+            },
+            y: {
+                grid: {
+                    display: true,
+                },
+                beginAtZero: true
+            }
+        },
+        maintainAspectRatio: false,
+        responsive: true
+    }
+});
+
+for (step = 0; step < size; step++) {
+    var tmptomorrow = new Date(today);
+    var tmptoday = new Date(today);
+    tmptomorrow.setDate(tmptomorrow.getDate() - step + 1);
+    tmptoday.setDate(tmptoday.getDate() - step);
+    tomorrowisostring = tmptomorrow.toISOString();
+    todayisostring = tmptoday.toISOString();
+    dates[size-step-1] = todayisostring.substring(8, 10);
+    var date_rqst = new XMLHttpRequest();
+    date_rqst.addEventListener('load', function () {
+        counts.unshift(parseInt(JSON.parse(this.responseText).count.replace(/\s/g, "")));
+        if (counts.length == size) {
+            chart.data.datasets.data = counts;
+            chart.update();
+        }
+    })
+    date_rqst.open('GET', 'https://lazyjobseeker.goatcounter.com/counter/TOTAL.json?start=' + todayisostring.slice(0, 10) + '&end=' + tomorrowisostring.slice(0, 10));
+    date_rqst.send();
+}
+
+var today = new Date();
+var yesterday = new Date(today)
+yesterday.setDate(yesterday.getDate() - 1)
+var today_cnt = new XMLHttpRequest();
+today_cnt.addEventListener('load', function() {
+    document.querySelector('#gc_today').innerText = JSON.parse(this.responseText).count.replace(/\s/g, "");
+})
+today_cnt.open('GET', 'https://lazyjobseeker.goatcounter.com/counter/TOTAL.json?start=' + today.toISOString().slice(0, 10));
+today_cnt.send()
+var yesterday_cnt = new XMLHttpRequest();
+yesterday_cnt.addEventListener('load', function() {
+    document.querySelector('#gc_yesterday').innerText = JSON.parse(this.responseText).count.replace(/\s/g, "");
+})
+yesterday_cnt.open('GET', 'https://lazyjobseeker.goatcounter.com/counter/TOTAL.json?start=' + yesterday.toISOString().slice(0, 10) + '&end=' + today.toISOString().slice(0, 10));
+yesterday_cnt.send();
+var total_cnt = new XMLHttpRequest();
+total_cnt.addEventListener('load', function() {
+    document.querySelector('#gc_total').innerText = JSON.parse(this.responseText).count.replace(/\s/g, "");
+})
+total_cnt.open('GET', 'https://lazyjobseeker.goatcounter.com/counter/TOTAL.json');
+total_cnt.send();
