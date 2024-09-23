d3063b72fd13fe3958a3d252416d31034b9e33c4
lazyjobseeker <diadee1990@gmail.com>
2024-09-23 13:46:01 +0900

    1) Bug Fix: Butler-Volmer 2) Polishing nomenclatures for JSXGraph items throughout blog

--- assets/js/custom-math-functions.js
+++ assets/js/custom-math-functions.js
@@ -1,12 +1,12 @@
 function sigmoid(k, x) {
     return 1 / (1 + Math.exp(-k*x));
-  };
+  }
 function step(k, a, b, x) {
   return sigmoid(k, x-a)*sigmoid(k, b-x);
-};
+  }
 function gibbs(k, x) {
-  return 0.5*x*x*step(1000, 0, k, x) + Math.max(k*(x-k)+0.5*k*k, 0)*sigmoid(1000, (x-k));
-};
+  return 0.5*x*x*sigmoid(1000, x)*sigmoid(1000, k-x) + Math.max(k*(x-k)+0.5*k*k, 0)*sigmoid(1000, (x-k));
+  }
 function parabola(x) {
   return x*x;
-};
+  }
