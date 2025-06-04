0f60ee5114b0ca64bc5658a26386d6bbb32dca00
lazyjobseeker-work <diadee1990@gmail.com>
2025-06-04 13:00:41 -0400

    Add unique IndexNow API key for Naver

--- indexnow.py
+++ indexnow.py
@@ -1,6 +1,6 @@
 import requests
 
-json_url = "https://lazyjobseeker.github.io/bing-indexnow-url-list.json"
+json_url = "https://lazyjobseeker.github.io/indexnow-url-list.json"
 
 response = requests.get(json_url)
 response.raise_for_status()  # fail fast if can't fetch
@@ -17,5 +17,13 @@ data = {
 headers = {'Content-Type': 'application/json; charset=utf-8'}
 r = requests.post("https://api.indexnow.org/IndexNow", json=data, headers=headers)
 print(r.status_code, r.text)
+
+data = {
+    "host": "https://lazyjobseeker.github.io/",
+    "key": "1b0522866f8b42049d0770c799fd11f5",
+    "keyLocation": "https://lazyjobseeker.github.io/1b0522866f8b42049d0770c799fd11f5.txt",
+    "urlList": url_list
+}
+
 r = requests.post("https://searchadvisor.naver.com/indexnow", json=data, headers=headers)
 print(r.status_code, r.text)
