fa6f2ccf7c788225a94a2c6b03972160dd8b4b5d
lazyjobseeker-work <diadee1990@gmail.com>
2025-06-09 20:30:32 -0400

    Test IndexNow for posts updated within 1-hr window

--- indexnow.py
+++ indexnow.py
@@ -7,32 +7,35 @@ response.raise_for_status()  # fail fast if can't fetch
 
 url_list = response.json()  # This will be a list of URLs
 
-for url in url_list:
-    print(url)
+if len(url_list) == 0:
+    print("No URLs found in the JSON file. Proceed without updating to IndexNow.")
+else:
+    for url in url_list:
+        print(url)
 
-data1 = {
-    "host": "lazyjobseeker.github.io",
-    "key": "7c8d916aed354f77bb94d77e66fa202e",
-    "keyLocation": "https://lazyjobseeker.github.io/7c8d916aed354f77bb94d77e66fa202e.txt",
-    "urlList": url_list
-}
+    data1 = {
+        "host": "lazyjobseeker.github.io",
+        "key": "7c8d916aed354f77bb94d77e66fa202e",
+        "keyLocation": "https://lazyjobseeker.github.io/7c8d916aed354f77bb94d77e66fa202e.txt",
+        "urlList": url_list
+    }
 
-headers1 = {'Content-Type': 'application/json; charset=utf-8'}
-r1 = requests.post("https://api.indexnow.org/IndexNow", json=data1, headers=headers1)
-print(r1.status_code, r1.text)
+    headers1 = {'Content-Type': 'application/json; charset=utf-8'}
+    r1 = requests.post("https://api.indexnow.org/IndexNow", json=data1, headers=headers1)
+    print(r1.status_code, r1.text)
 
-response2 = requests.get(json_url)
-response2.raise_for_status()  # fail fast if can't fetch
+    response2 = requests.get(json_url)
+    response2.raise_for_status()  # fail fast if can't fetch
 
-url_list2 = response2.json()  # This will be a list of URLs
+    url_list2 = response2.json()  # This will be a list of URLs
 
-data2 = {
-    "host": "lazyjobseeker.github.io",
-    "key": "988e928790a24ceb80bd704f7f067aaf",
-    "keyLocation": "https://lazyjobseeker.github.io/988e928790a24ceb80bd704f7f067aaf.txt",
-    "urlList": url_list2
-}
+    data2 = {
+        "host": "lazyjobseeker.github.io",
+        "key": "988e928790a24ceb80bd704f7f067aaf",
+        "keyLocation": "https://lazyjobseeker.github.io/988e928790a24ceb80bd704f7f067aaf.txt",
+        "urlList": url_list2
+    }
 
-headers2 = {'Content-Type': 'application/json; charset=utf-8'}
-r2 = requests.post("https://searchadvisor.naver.com/indexnow", json=data2, headers=headers2)
-print(r2.status_code, r2.text)
+    headers2 = {'Content-Type': 'application/json; charset=utf-8'}
+    r2 = requests.post("https://searchadvisor.naver.com/indexnow", json=data2, headers=headers2)
+    print(r2.status_code, r2.text)
