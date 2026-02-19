6d2411f780c46094e14cedbf854d18f27bac80dc
lazyjobseeker-work <diadee1990@gmail.com>
2026-02-19 12:10:26 -0500

    resolving reference issue

--- assets/js/visitorstat.js
+++ assets/js/visitorstat.js
@@ -1,6 +1,6 @@
 // Visitor Statistics
 const jsonFile_pv = document.getElementById("myPageviewCountChart").dataset.json;
-const jsonFile_totalpv = "assets/json/gctotalpageviews.json";
+const jsonFile_totalpv = "https://lazyjobseeker.github.io/assets/json/gctotalpageviews.json";
 
 fetch(jsonFile_totalpv)
   .then(res => res.json())
