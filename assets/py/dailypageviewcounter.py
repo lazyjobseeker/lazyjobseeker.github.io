b56dc5438f6a090f1ce11eb1f0125e2600ec92e0
lazyjobseeker-work <diadee1990@gmail.com>
2026-02-19 19:42:28 -0500

    pvcounter updates yesterdays' too - sync issue

--- assets/py/dailypageviewcounter.py
+++ assets/py/dailypageviewcounter.py
@@ -6,7 +6,19 @@ import datetime as dt
 PAGEVIEW_FILE = Path("assets/json/gcdailypageviews.json")
 PAGEVIEW_TOTAL_FILE = Path("assets/json/gctotalpageviews.json")
 
-def fetch_pageviews_daily():
+def fetch_pageviews_yesterday():
+    # Replace with your actual API endpoint and parameters
+    today = dt.date.today().isoformat()
+    yesterday = (dt.date.today() - dt.timedelta(days=1)).isoformat()
+    url = f"https://slothsattic.goatcounter.com/counter/TOTAL.json?start={yesterday}&end={today}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()  # Assuming the API returns JSON data
+    else:
+        print(f"Failed to fetch pageviews: {response.status_code}")
+        return {}
+
+def fetch_pageviews_today():
     # Replace with your actual API endpoint and parameters
     today = dt.date.today().isoformat()
     tomorrow = (dt.date.today() + dt.timedelta(days=1)).isoformat()
@@ -28,6 +40,8 @@ def fetch_pageviews_total():
         print(f"Failed to fetch pageviews: {response.status_code}")
         return {}
 
+yesterday = dt.date.today() - dt.timedelta(days=1)
+yesterday = yesterday.isoformat()
 
 today = dt.date.today().isoformat()
 data = {}
@@ -35,7 +49,8 @@ if PAGEVIEW_FILE.exists():
     with open(PAGEVIEW_FILE, 'r', encoding='utf-8') as f:
         data = json.loads(PAGEVIEW_FILE.read_text(encoding='utf-8'))
 
-data[today] = int(fetch_pageviews_daily().get("count", 0))
+data[yesterday] = int(fetch_pageviews_yesterday().get("count", 0))
+data[today] = int(fetch_pageviews_today().get("count", 0))
 
 with open(PAGEVIEW_FILE, 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
